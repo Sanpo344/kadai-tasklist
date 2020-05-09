040dbbc7b5c8c6952b2e86547b59d261baad625e
sanpo.k <sanpo344@gmail.com>
2020-05-09 12:13:50 +0000

    kadai-tasklist2

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,16 +45,14 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
         ]);
         
-        // $this->validate($request, [
-        //     'status' => 'required|max:10',
-        // ]);
-        
-        $message = new Task;
-        $message->content = $request->content;
-        $message->save();
+        $task = new Task;
+        $task->status = $request->status;
+        $task->content = $request->content;
+        $task->save();
         
         return redirect('/');
     }
@@ -99,14 +97,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
         ]);
         
-        // $this->validate($request, [
-        //     'status' => 'required|max:10',
-        // ]);
-        
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
